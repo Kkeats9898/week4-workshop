5d5654602bf03f29d160e62b9bcf014fe04c9c69
Kena Keats <kenepurukeats@outlook.com>
2025-08-01 13:45:45 +1000

    Add new components to route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root', // HTML tag for adding this component to a page
+  standalone: true,
+  imports: [RouterOutlet, LoginComponent, AccountComponent, RouterLink],
+  templateUrl: './app.component.html', // HTML to be displayed in component
+  styleUrl: './app.component.css' // CSS to style component
 })
 export class AppComponent {
   title = 'week4tut';
